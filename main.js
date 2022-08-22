641c1a8c9b7440605bb636ef4f6773d30698fcec
AaryanTeckchandani <96259203+AaryanTeckchandani@users.noreply.github.com>
2022-08-22 13:28:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
-canvas= document.getElementById("myCanvas");
-colour="blue";
+canvas= document.getElementById("myLogo");
+colour="red";
 ctx= canvas.getContext("2d");
+ctx.beginPath();
 ctx.strokeStyle = colour;
 ctx.lineWidth=2;
-ctx.arc(200, 200, 40, 0, 2 *Math.PI);
+ctx.arc(500, 200, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+colour="black";
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = colour;
+ctx.lineWidth=2;
+ctx.arc(300, 200, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+colour="blue";
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = colour;
+ctx.lineWidth=2;
+ctx.arc(400, 200, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+colour="yellow";
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = colour;
+ctx.lineWidth=2;
+ctx.arc(350, 250, 40, 0, 2 *Math.PI);
+ctx.stroke();
+
+colour="green";
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = colour;
+ctx.lineWidth=2;
+ctx.arc(450, 250, 40, 0, 2 *Math.PI);
 ctx.stroke();
